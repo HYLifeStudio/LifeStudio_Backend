f10769d43dd7779f8f85e3e2ac73ba921b86579c
sgwon96 <zxcvb5434@likelion.org>
2021-07-27 00:53:15 +0900

    [feat#13] Spring Security 적용 및 비밀번호 암호화

--- backend/src/main/java/lifestudio/backend/domain/user/service/UserService.java
+++ backend/src/main/java/lifestudio/backend/domain/user/service/UserService.java
@@ -2,6 +2,8 @@ package lifestudio.backend.domain.user.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,8 +18,12 @@ public class UserService {
 
 	private final UserRepository userRepository;
 
+	private final PasswordEncoder passwordEncoder;
+
 	@Transactional
 	public Long createUser(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 		userRepository.save(user);
 		return user.getId();
 	}
